d35cacac587b4775f2cad39ee9469023d8fc8147
Dinh Nghia <nghia15121991@gmail.com>
2021-07-02 15:01:46 +0700

    Update theme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # coco-template
 Mẫu css cho website
+
+Yêu cầu:
+- Đã cài đặt nodejs
+- Chạy lệnh: npm install -g gulp-cli
+- Chạy lệnh: npm install
+
+Trên cmd trong thư mục gõ: gulp
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,9 @@
 $("body").removeClass("preload");
 const swiper_banner = new Swiper(".swiper-banner", {
   loop: true,
+  autoplay: {
+    delay: 4000,
+  },
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
@@ -33,3 +36,56 @@ $(".hambuger-sidebar-mobile").click(function () {
 $("#menu .sidebar-overlay").click(function () {
   $("#menu").removeClass("open-sidebar");
 });
+const swiper_posts = new Swiper(".swiper-posts", {
+  loop: true,
+  autoplay: {
+    delay: 4000,
+  },
+  // Navigation arrows
+  slidesPerView: 2,
+  spaceBetween: 10,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//Scroll fixed
+// Define the menu we are working with
+var menu = $('.header-main');
+
+// Get the menus current offset
+var origOffsetY = menu.offset().top;
+
+/**
+ * scroll
+ * Perform our menu mod
+ */
+function scroll() {
+
+    // Check the menus offset. 
+    if ($(window).scrollTop() >= origOffsetY) {
+
+        //If it is indeed beyond the offset, affix it to the top.
+        $(menu).addClass('fixed-top bg-fixed');
+
+    } else {
+
+        // Otherwise, un affix it.
+        $(menu).removeClass('fixed-top bg-fixed');
+
+    }
+}
+
+// Anytime the document is scrolled act on it
+document.onscroll = scroll;
